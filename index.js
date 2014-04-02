c14d338fac4cf19aa2793fab9c7309d08e5cf51b
Masaaki Morishita <ikasama3.0@gmail.com>
2014-04-03 00:28:50 +0900

    binding complete

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 module.exports = function (ast, rework) {
+
+  var newSels = [];
+  var allSels = [];
+
+  ast.rules.forEach(function visit(rule) {
+    rule.selectors.forEach(function (allSel) {
+      if (allSel.match(/^\(.+?\)/)) {
+        allSel  = allSel.slice(1, -1);
+      }
+      Array.prototype.push.apply(allSels, [allSel]);
+    });
+  });
+
   ast.rules.forEach(function visit(rule) {
     if (rule.rules)  {
       rule.rules.forEach(visit);
@@ -6,15 +19,32 @@ module.exports = function (ast, rework) {
 
     if (!rule.selectors) return;
 
-    var newSels = [];
-
     rule.selectors.forEach(function (sel) {
       if (sel.match(/^\(.+?\)/)) {
-        var arr = sel.slice(1, -1);
-        sel = [arr];
-        Array.prototype.push.apply(newSels, sel);
+        var s = sel.slice(1, -1);
+        Array.prototype.push.apply(newSels, [s]);
+        if (check(s)) {
+          rule.selectors = [s];
+        } else {
+          throw new Error('rework-binding: Compile Error !!');
+        }
       }
     });
-    rule.selectors = newSels;
   });
+
+  function check(sel) {
+    var counter = 0;
+    if (allSels.length > 0) {
+      for (var i = 0; i < allSels.length; i++) {
+        if (allSels[i] == sel) {
+          counter++;
+        }
+      }
+      if (counter > 1) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
 };
