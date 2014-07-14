f268856fb96165b15049b15bfcf0ac3fd6bedf2b
Masaaki Morishita <ikasama3.0@gmail.com>
2014-07-14 18:15:54 +0900

    fix rework.use() order

--- index.js
+++ index.js
@@ -1,66 +1,68 @@
+var inspect = require('obj-inspector')
 module.exports = function (ast, rework) {
+    // inspect(ast)
 
-  var newSels = [];
-  var allSels = [];
+    var newSels = [];
+    var allSels = [];
 
-  ast.rules.forEach(function visit(rule) {
-    rule.selectors.forEach(function (allSel) {
-      if (allSel.match(/^\(.+?\)/)) {
-        allSel = allSel.slice(1, -1);
-      }
-      if (allSel.match(/^\%.+?/)) {
-        allSel = allSel.slice(1);
-      }
-      Array.prototype.push.apply(allSels, [allSel]);
+    ast.rules.forEach(function visit(rule) {
+        rule.selectors.forEach(function (allSel) {
+            if (allSel.match(/^\(.+?\)/)) {
+                allSel = allSel.slice(1, -1);
+            }
+            if (allSel.match(/^\%.+?/)) {
+                allSel = allSel.slice(1);
+            }
+            Array.prototype.push.apply(allSels, [allSel]);
+        });
     });
-  });
 
-  ast.rules.forEach(function visit(rule) {
-    if (rule.rules)  {
-      rule.rules.forEach(visit);
-    }
-
-    if (!rule.selectors) return;
-
-    rule.selectors.forEach(function (sel, index, arr) {
-      if (sel.match(/^\(.+?\)/)) {
-        var s = sel.slice(1, -1);
-        Array.prototype.push.apply(newSels, [s]);
-        if (check(s)) {
-          arr[index] = s;
-        } else {
-          var err =  new Error('rework-rule-binding: binding-selector must not cascade');
-          err.position = rule.declarations.position;
-          throw err;
+    ast.rules.forEach(function visit(rule) {
+        if (rule.rules)  {
+            rule.rules.forEach(visit);
         }
-      }
-      if (sel.match(/^\%.+?/)) {
-        var s = sel.slice(1);
-        Array.prototype.push.apply(newSels, [s]);
-        if (check(s)) {
-          arr[index] = s;
-        } else {
-          var err = new Error('rework-rule-binding: placeholder selector must not cascade');
-          err.position = rule.declarations.position;
-          throw err;
-        }
-      }
+
+        if (!rule.selectors) return;
+
+        rule.selectors.forEach(function (sel, index, arr) {
+            if (sel.match(/^\%/)) {
+                var s = sel.slice(1);
+                Array.prototype.push.apply(newSels, [s]);
+                if (check(s)) {
+                    arr[index] = s;
+                } else {
+                    var err = new Error('rework-rule-binding: placeholder selector must not cascade');
+                    err.position = rule.declarations.position;
+                    throw err;
+                }
+            }
+            if (sel.match(/^\(.+?\)/)) {
+                var s = sel.slice(1, -1);
+                Array.prototype.push.apply(newSels, [s]);
+                if (check(s)) {
+                    arr[index] = s;
+                } else {
+                    var err =  new Error('rework-rule-binding: binding-selector must not cascade');
+                    err.position = rule.declarations.position;
+                    throw err;
+                }
+            }
+        });
     });
-  });
 
-  function check(sel) {
-    var counter = 0;
-    if (allSels.length > 0) {
-      for (var i = 0; i < allSels.length; i++) {
-        if (allSels[i] == sel) {
-          counter++;
+    function check(sel) {
+        var counter = 0;
+        if (allSels.length > 0) {
+            for (var i = 0; i < allSels.length; i++) {
+                if (allSels[i] == sel) {
+                    counter++;
+                }
+            }
+            if (counter > 1) {
+                return false;
+            } else {
+                return true;
+            }
         }
-      }
-      if (counter > 1) {
-        return false;
-      } else {
-        return true;
-      }
     }
-  }
 };
--- test/test.js
+++ test/test.js
@@ -11,8 +11,8 @@ function fixture(name) {
 function compareFixtures(name) {
   return expect(
     rework(fixture(name))
-    .use(extend)
     .use(bind)
+    .use(extend)
     .toString().trim()
   ).to.equal(fixture(name + '.out'));
 }
@@ -36,14 +36,20 @@ describe('rework-rule-binding', function() {
 
   it('throw error when cascade binding-selector', function() {
     var output = function() {
-      return rework(fixture('error-cascading')).use(extend).use(bind).toString();
+      return rework(fixture('error-cascading'))
+             .use(bind)
+             .use(extend)
+             .toString();
     };
     expect(output).to.Throw(Error, 'rework-rule-binding: binding-selector must not cascade');
   });
 
   it('throw error when cascade placeholder selector', function() {
     var output = function() {
-      return rework(fixture('placeholder')).use(extend).use(bind).toString();
+      return rework(fixture('placeholder'))
+             .use(bind)
+             .use(extend)
+             .toString();
     };
     expect(output).to.Throw(Error, 'rework-rule-binding: placeholder selector must not cascade');
   })
